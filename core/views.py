9a506959f2121922d556c07d0692a4a65b34176b
VaveyNL <vaveynlp@gmail.com>
2026-01-11 15:13:41 +0500

    Настроены urls, шаблоны и базовые страницы

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "core/index.html")
+
+
+def about(request):
+    return render(request, "core/about.html")
+
+
+def login_view(request):
+    return render(request, "core/login.html")
+
+
+def profile(request):
+    return render(request, "core/profile.html")
+
+
+def create_run_plan(request):
+    return render(request, "core/create_run_plan.html")
+
+
+def recommendation_list(request):
+    return render(request, "core/recommendation_list.html")
